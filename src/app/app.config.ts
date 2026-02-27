d243271f04072cdbd56ac23ae5736aceb2ad6c4f
MortalSecond <irondiego_jkl@hotmail.com>
2026-02-27 15:37:56 -0600

    Implement ngx-translate

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,23 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessC
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideTranslateService } from '@ngx-translate/core';
+import { provideHttpClient } from '@angular/common/http';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(),
+    provideTranslateService({
+      loader: provideTranslateHttpLoader({
+        prefix: './assets/i18n/',
+        suffix: '.json'
+      }),
+      fallbackLang: 'en',
+      lang: 'en'
+    })
   ]
 };
--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
+  // Services
+  private readonly translateService = inject(TranslateService);
 
+  // Functional Methods
+  switchLang(){
+    if(this.translateService.getCurrentLang() == "es")
+      this.translateService.use('en');
+    else
+      this.translateService.use('es');
+  }
 }
