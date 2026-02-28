97f9af5a450882987985abc67fa5e4658ed8bb5e
MortalSecond <irondiego_jkl@hotmail.com>
2026-02-27 23:38:44 -0600

    Add the translate pipeline to the landing page.

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Navbar } from "../../shared/components/navbar/navbar";
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-home',
-  imports: [Navbar],
+  imports: [Navbar, TranslateModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
